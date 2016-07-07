6230c11b6d4ad247f828c1314e822a4cab3375a1
Amanda Thorne <manda.thorne@gmail.com>
2016-07-06 22:17:17 -0500

    code comments amended

--- main.js
+++ main.js
@@ -19,9 +19,11 @@ $.ajax({
       //appending li's and deletebutton to ul
       //DELETE request
       deleteButton.on('click', function() {
+        //when user clicks on 'delete' button...
         $(li).hide();
         //hide li when delete button is clicked
       $.ajax({
+        //DELETE from server
         type: 'DELETE',
         url: 'http://tiny-za-server.herokuapp.com/collections/amanda/'+ item._id,
         dataType: 'json',
@@ -37,8 +39,11 @@ $.ajax({
 });
 
 addButton.on('click', function() {
+  //when user clicks on 'add' button...
   // console.log($('.task').val());
+  console.log($('.task'));
   $.ajax({
+    //POST to server
     type: 'POST',
     dataType: 'json',
     url: 'http://tiny-za-server.herokuapp.com/collections/amanda',
@@ -46,10 +51,11 @@ addButton.on('click', function() {
       console.log(response);
         var li = $('<li></li>');
         var deleteButton = $('<input class="delete" type="button" value="Delete">');
-        var liText = li.text('•' + ' ' + $('.task').val());
+        li.text('•' + ' ' + $('.task').val());
+        //task.val same as li.text above in get request
         $('.compiled-items').append(li);
+        //append newly added item to ul
         li.append(deleteButton);
-        // $('.compiled-items').append($('.task').val());
         deleteButton.on('click', function(item) {
           $(li).hide();
           //hide li when delete button is clicked
@@ -58,5 +64,9 @@ addButton.on('click', function() {
     data: {
       task: $('.task').val()
     }
+    //this data is sent to server when 'add' button is clicked
   });
 });
+
+//declare delete function in global to call in both requests.
+//had trouble with finding 'item._id' in global. only works under forEach function
